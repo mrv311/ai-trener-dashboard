9dbf01ed805db974d5dcd7568aa9623d55d69376
Dino Malbasa <Dino.Malbasa@mzozt.hr>
2026-04-07 14:05:45 +0200

    Verzija 3.64 anaerobni faktor dole

--- src/utils/workoutParser.js
+++ src/utils/workoutParser.js
@@ -121,9 +121,9 @@ function calculateCategoryDifficulty(steps, category) {
       score = 1.0 + (tizMinutes / 15) * 4.0; 
       break;
     case 'Anaerobni':
-      // Anaerobni treninzi jako brzo idu u opasne visine za mali TiZ
-      // Postavljamo veći djelitelj i eksponencijalno rezanje dužih treninga
-      score = 1.0 + Math.pow(tizMinutes / 8, 0.85) * 5.0;
+      // Ekstremni treninzi tipa Taylor -2 imaju preko 30 minuta TiZ
+      // Još strože i ravnije po krivulji kako ne bi bježalo iznad 15
+      score = 1.0 + Math.pow(tizMinutes / 10, 0.8) * 4.5;
       break;
     default:
       score = (totalTSS / 60) * 4.0; 
